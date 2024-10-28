5b160d5b78026f861b2980de9fb2507bf148b5c4
49 <laythandamin@gmail.com>
2024-10-28 11:48:21 -0500

    Cleaned up files and added some stuff (idk what I added)

---
+++ JS/hackedEffect.js
@@ -0,0 +1,31 @@
+const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+let interval = null;
+
+document.addEventListener("DOMContentLoaded", () => {
+  document.querySelectorAll("a").forEach((link) => {
+    link.onmouseover = (event) => {
+      let iteration = 0;
+
+      clearInterval(interval);
+
+      interval = setInterval(() => {
+        event.target.innerText = event.target.innerText
+          .split("")
+          .map((letter, index) => {
+            if (index < iteration) {
+              return event.target.dataset.value[index];
+            }
+            return letters[Math.floor(Math.random() * 26)];
+          })
+          .join("");
+
+        if (iteration >= event.target.dataset.value.length) {
+          clearInterval(interval);
+        }
+
+        iteration += 1 / 3;
+      }, 50);
+    };
+  });
+});
